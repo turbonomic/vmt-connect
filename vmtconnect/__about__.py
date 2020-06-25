c6fb26cbd9e0d5d6b0aa1355dc2fff9e4831c800
R.A. Stern <rastern@users.noreply.github.com>
2020-06-25 10:10:13 -0400

    Bump version for release

--- vmtconnect/__about__.py
+++ vmtconnect/__about__.py
@@ -13,7 +13,7 @@
 
 __title__ = 'vmtconnect'
 __description__ = 'Turbonomic API Client'
-__version__ = '3.2.6.dev2'
+__version__ = '3.2.6'
 __author__ = 'R.A. Stern'
 __author_email__ = 'richard.stern@turbonomic.com'
 __license__ = 'Apache 2.0'
